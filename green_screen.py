134bf0d3279a23566e9b5baf9afc3c327dfc494c
Danner Pacheco <djpachecoontiveros@miners.utep.edu>
2018-11-07 19:13:43 -0700

    prototype find green

--- green_screen.py
+++ green_screen.py
@@ -1,6 +1,9 @@
 import numpy as np 
 from scipy import misc
 import matplotlib.pyplot as plt
+from enum import Enum
+
+
 
 
 def main():
@@ -15,7 +18,7 @@ def main():
     print(foreground_image.shape)
     print(background_image.shape)
     
-    #is_green = find_green(foreground_image)
+    is_green = find_green(foreground_image)
     
     #final_image = merge_image(foreground_image, background_image, is_green)
     
@@ -36,13 +39,31 @@ def read_image(path):
 
 
 def find_green(image):
+
+    
+    shape = image.shape
+    shape = shape[0:2]
+    is_green = np.zeros(shape)
+    
+    x_coord = 0
+    y_coord = 0
     
     
+    for x in range(len(image)):
+        x_coord += 1
+        
+        for y in range(len(image[x])):
+            y_coord += 1
+            
+            if(image[x][y][0] < 5 and image[x][y][1] > 245 and image[x][y][2] < 5):
+                is_green[x][y] = 1
     
     #returns boolean array with true in green areas
-    return
+    return is_green
 
 def merge_image(image, background, green_area):
+    
+    
     
     
     
